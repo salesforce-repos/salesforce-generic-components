503227ad7cf6573635b478852a49be3a6353cdb6
salesforce-repos <sushantrokade241@gmail.com>
2024-05-14 19:16:34 +0530

    generic query component

--- force-app/main/default/lwc/dynamicFilter/dynamicFilter.js
+++ force-app/main/default/lwc/dynamicFilter/dynamicFilter.js
@@ -1,10 +1,147 @@
 import { LightningElement, api, wire, track } from "lwc";
+import { getPicklistValues } from "lightning/uiObjectInfoApi";
+import getPickListvaluesByFieldName from "@salesforce/apex/QueryEditorController.getOptionsForSelectedPicklistField";
 
 export default class DynamicFilter extends LightningElement {
   @api objectApiName; // Input property to accept custom object API name
   @api fieldOptions; // Input property to accept fields information
   @api fieldDetails;
   @api selectedFields;
+  @api objectInfo;
+
+  @track currentPicklistField;
+  currentIndex;
+
+  @track logicOptions = [
+    { label: "AND", value: "AND" },
+    { label: "OR", value: "OR" },
+    { label: "Custom", value: "Custom" }
+  ];
+
+  selectedLogicOption = "AND";
+  customLogic;
+
+  operations = [
+    {
+      label: "equals",
+      value: " = ",
+      types:
+        "String,Picklist,Url,Email,TextArea,Reference,Phone,Date,DateTime,Currency,Double,Boolean,Int,Address,NestedField"
+    },
+    {
+      label: "not equals",
+      value: " != ",
+      types:
+        "String,Picklist,Url,Email,TextArea,Reference,Phone,Date,DateTime,Currency,Double,Boolean,Int,Address,NestedField"
+    },
+    {
+      label: "less than",
+      value: " < ",
+      types: "Date,DateTime,Currency,Double,Int,Address,NestedField"
+    },
+    {
+      label: "greater than",
+      value: " > ",
+      types: "Date,DateTime,Currency,Double,Int,Address,NestedField"
+    },
+    {
+      label: "less than or equals",
+      value: " <= ",
+      types: "Date,DateTime,Currency,Double,Int,Address,NestedField"
+    },
+    {
+      label: "greater than or equals",
+      value: " >= ",
+      types: "Phone,Date,DateTime,Currency,Double,Int,Address,NestedField"
+    },
+    {
+      label: "LIKE",
+      value: " LIKE ",
+      types: "String,Url,Email,Reference,Phone,NestedField"
+    },
+    {
+      label: "NOT IN",
+      value: " NOT IN ",
+      types: "String,Url,Email,Reference,Phone,Currency,,Address,NestedField"
+    },
+    {
+      label: "IN",
+      value: " IN ",
+      types: "String,Url,Email,Phone,Currency,Address,NestedField"
+    }
+  ];
+
+  fieldTypeSettings = {
+    String: {
+      inputType: "text",
+      dataTransformationFunction: "wrapInQuotes",
+      isType: true
+    },
+    Picklist: {
+      inputType: "text",
+      dataTransformationFunction: "wrapInQuotes",
+      isType: false,
+      showPicklistInput: true
+    },
+    Email: {
+      inputType: "text",
+      dataTransformationFunction: "wrapInQuotes",
+      isType: true
+    },
+    Currency: {
+      inputType: "number",
+      dataTransformationFunction: null,
+      isType: true
+    },
+    Address: {
+      inputType: null,
+      dataTransformationFunction: null,
+      isType: true
+    },
+    Double: {
+      inputType: "number",
+      dataTransformationFunction: null,
+      isType: true
+    },
+    TextArea: {
+      inputType: "text",
+      dataTransformationFunction: "wrapInQuotes",
+      isType: true
+    },
+    Reference: {
+      inputType: "text",
+      dataTransformationFunction: "wrapInQuotes",
+      isType: true
+    },
+    DateTime: {
+      inputType: "datetime",
+      dataTransformationFunction: null,
+      isType: true
+    },
+    Phone: {
+      inputType: "text",
+      dataTransformationFunction: "wrapInQuotes",
+      isType: true
+    },
+    Boolean: {
+      inputType: "checkbox",
+      dataTransformationFunction: "transformBoolean",
+      isType: false,
+      showBooleanInput: true
+    },
+    Date: { inputType: "date", dataTransformationFunction: null, isType: true },
+    Int: {
+      inputType: "number",
+      dataTransformationFunction: null,
+      isType: true
+    },
+    Url: {
+      inputType: "url",
+      dataTransformationFunction: "wrapInQuotes",
+      isType: true
+    }
+  };
+
   @track filters = [
     {
       sequence: 1,
@@ -16,31 +153,34 @@ export default class DynamicFilter extends LightningElement {
       showPicklistInput: false,
       showBooleanInput: false,
       options: [],
+      booleanOptions: [],
       selectedValues: [],
       operatorOptions: [],
       isType: true,
       formatter: "",
-    },
+      operatorDisabled: true,
+      valueDisabled: true
+    }
   ]; // Array to store filter objects { field, operator, value, condition }
   soqlQuery;
   conditionOptions = [
     { label: "AND", value: "AND" },
-    { label: "OR", value: "OR" },
+    { label: "OR", value: "OR" }
   ];
 
   defaultOperators = [
     { label: "Equals", value: "=" },
-    { label: "Not Equals", value: "!=" },
+    { label: "Not Equals", value: "!=" }
   ];
 
   piclistOperators = [
     { label: "Equals", value: "IN" },
-    { label: "Not Equals", value: "NOT IN" },
+    { label: "Not Equals", value: "NOT IN" }
   ];
 
   booleanOptions = [
     { label: "True", value: "true" },
-    { label: "False", value: "false" },
+    { label: "False", value: "false" }
   ];
 
   numberOperatorOptions = [
@@ -49,7 +189,7 @@ export default class DynamicFilter extends LightningElement {
     { label: "Greater Than", value: ">" },
     { label: "Less Than", value: "<" },
     { label: "Greater Than or Equal To", value: ">=" },
-    { label: "Less Than or Equal To", value: "<=" },
+    { label: "Less Than or Equal To", value: "<=" }
     // Add more number operator options as needed
   ];
 
@@ -57,7 +197,7 @@ export default class DynamicFilter extends LightningElement {
     { label: "Equals", value: "=" },
     { label: "Not Equals", value: "!=" },
     { label: "LIKE", value: "LIKE" },
-    { label: "NOT LIKE", value: "NOT LIKE" },
+    { label: "NOT LIKE", value: "NOT LIKE" }
     // Add more string operator options as needed
   ];
 
@@ -67,7 +207,7 @@ export default class DynamicFilter extends LightningElement {
     { label: "Greater Than", value: ">" },
     { label: "Less Than", value: "<" },
     { label: "Greater Than or Equal To", value: ">=" },
-    { label: "Less Than or Equal To", value: "<=" },
+    { label: "Less Than or Equal To", value: "<=" }
     // Add more date operator options as needed
   ];
 
@@ -77,13 +217,37 @@ export default class DynamicFilter extends LightningElement {
     { label: "Greater Than", value: ">" },
     { label: "Less Than", value: "<" },
     { label: "Greater Than or Equal To", value: ">=" },
-    { label: "Less Than or Equal To", value: "<=" },
+    { label: "Less Than or Equal To", value: "<=" }
     // Add more datetime operator options as needed
   ];
+
+  async getPicklistValuesForSelectedPicklistField() {
+    await getPickListvaluesByFieldName({
+      selectedObjectName: this.objectApiName,
+      selectedField: this.currentPicklistField
+    })
+      .then((result) => {
+        if (result) {
+          this.filters[this.currentIndex].options = result;
+          this.filters[this.currentIndex].valueDisabled = false;
+          console.log(
+            "@@ result " +
+              JSON.stringify(this.filters[this.currentIndex].options)
+          );
+        }
+      })
+      .catch((error) => {
+        console.error("Error in getting pickist values " + error);
+      });
+  }
 
   connectedCallback() {
     // Initialize with one empty filter object
     //this.addFilter();
+  }
+
+  get isCustomLogic() {
+    return this.selectedLogicOption === "Custom" ? true : false;
   }
 
   addFilter() {
@@ -96,11 +260,14 @@ export default class DynamicFilter extends LightningElement {
       fieldType: "",
       showPicklistInput: false,
       showBooleanInput: false,
+      booleanOptions: [],
       options: [],
       selectedValues: [],
       operatorOptions: [],
       isType: true,
       formatter: "",
+      operatorDisabled: true,
+      valueDisabled: true
     });
   }
 
@@ -144,8 +311,56 @@ export default class DynamicFilter extends LightningElement {
     this.filters[index].field = value;
 
     // Get field type
-    const fieldDetail = this.fieldDetails.find((field) => field.name === value);
-    this.checkDataType(fieldDetail, index);
+    const fieldDetail = this.fieldDetails.find(
+      (field) => field.value === value
+    );
+
+    console.log("@@ fieldDetail " + JSON.stringify(fieldDetail));
+    if (fieldDetail) {
+      try {
+        this.filters[index].fieldType =
+          this.fieldTypeSettings[fieldDetail.dataType].inputType;
+
+        this.filters[index].operatorOptions = this.operations.filter(
+          (operation) => operation.types.includes(fieldDetail.dataType)
+        );
+
+        this.filters[index].operatorDisabled = false;
+
+        this.filters[index].isType =
+          this.fieldTypeSettings[fieldDetail.dataType].isType;
+
+        this.filters[index].showPicklistInput =
+          this.fieldTypeSettings[fieldDetail.dataType]?.hasOwnProperty(
+            "showPicklistInput"
+          ) || false;
+
+        if (this.filters[index].showPicklistInput) {
+          if (this.filters[index].options.length === 0) {
+            this.currentIndex = index;
+            this.currentPicklistField = this.filters[index].field;
+            this.getPicklistValuesForSelectedPicklistField();
+            console.log("@@ current " + this.currentPicklistField);
+            console.log("@ obj in dynamic " + JSON.stringify(this.objectInfo));
+          }
+          //this.filters[index].options = fieldDetail.picklistOptions;
+        } else {
+          this.filters[index].valueDisabled = false;
+        }
+
+        this.filters[index].showBooleanInput =
+          this.fieldTypeSettings[fieldDetail.dataType]?.hasOwnProperty(
+            "showBooleanInput"
+          ) || false;
+
+        if (this.filters[index].showBooleanInput) {
+          this.filters[index].booleanOptions = this.booleanOptions;
+        }
+      } catch (err) {
+        console.error(err);
+      }
+    }
+    //this.checkDataType(fieldDetail, index);
   }
 
   handleOperatorChange(event) {
@@ -163,62 +378,82 @@ export default class DynamicFilter extends LightningElement {
     this.filters[event.currentTarget.dataset.index].condition = value;
   }
 
+  @api
   generateSOQLQuery() {
-    let soql =
-      "SELECT Id, " + this.selectedFields + " FROM " + this.objectApiName + " ";
-    if (this.filters && this.filters.length > 0) {
-      soql += `WHERE `;
-    }
-    this.filters.forEach((filter, index) => {
-      if (index > 0) {
-        soql += ` ${filter.condition} `;
-      }
-      if (
-        filter.field &&
-        filter.operator &&
-        (filter.value || filter.selectedValues.length > 0)
-      ) {
-        if (filter.showPicklistInput) {
-          const values = filter.selectedValues.join("', '");
-          soql += `${filter.field} ${filter.operator} ('${values}')`;
-        } else if (filter.fieldType === "text") {
-          soql += `${filter.field} ${filter.operator} ${this.formatClause(
-            filter.field,
-            filter.operator,
-            filter.value,
-            "text"
-          )}`;
-        } else if (filter.showBooleanInput) {
-          const booleanValue = filter.value === "true" ? true : false;
-          soql += `${filter.field} ${filter.operator} ${booleanValue}`;
-        } else {
-          soql += `${filter.field} ${filter.operator} ${filter.value}`;
+    console.log("@@ called");
+    try {
+      let showWhere = false;
+      //let soql =
+      //"SELECT Id, " + this.selectedFields + " FROM " + this.objectApiName + " ";
+      let soqlWhere = "";
+      if (!this.isCustomLogic) {
+        if (this.filters && this.filters.length > 0) {
+          //showWhere = true;
+          //oql += `WHERE `;
         }
+        this.filters.forEach((filter, index) => {
+          if (index > 0) {
+            soqlWhere += ` ${this.selectedLogicOption} `;
+          }
+          if (
+            filter.field &&
+            filter.operator &&
+            (filter.value || filter.selectedValues.length > 0)
+          ) {
+            if (index === 0) {
+              showWhere = true;
+            }
+
+            if (filter.showPicklistInput) {
+              const values = filter.selectedValues.join("', '");
+              let operator = filter.operator === " = " ? "IN" : "NOT IN";
+
+              soqlWhere += `${filter.field} ${operator} ('${values}')`;
+            } else if (filter.fieldType === "text") {
+              soqlWhere += `${filter.field} ${filter.operator} ${this.formatClause(
+                filter.field,
+                filter.operator,
+                filter.value,
+                "text"
+              )}`;
+            } else if (filter.showBooleanInput) {
+              const booleanValue = filter.value === "true" ? true : false;
+              soqlWhere += `${filter.field} ${filter.operator} ${booleanValue}`;
+            } else {
+              soqlWhere += `${filter.field} ${filter.operator} ${filter.value}`;
+            }
+          }
+        });
+        this.soqlQuery = soqlWhere;
+
+        // Check if there is anything after "WHERE"
+        /*let whereIndex = this.soqlQuery.indexOf("WHERE ");
+        if (
+          whereIndex !== -1 &&
+          whereIndex + "WHERE ".length !== this.soqlQuery.length
+        ) {
+          // If there is something after "WHERE", do nothing
+        } else {
+          // If there is nothing after "WHERE", remove "WHERE" from the string
+          this.soqlQuery = this.soqlQuery.replace(" WHERE", "");
+        }*/
+
+        console.log("@@ before event");
+
+        this.dispatchEvent(
+          new CustomEvent("fetchresults", {
+            detail: {
+              payload: {
+                whereClaue: soqlWhere,
+                showWhere: showWhere
+              }
+            }
+          })
+        );
       }
-    });
-    this.soqlQuery = soql;
-
-    // Check if there is anything after "WHERE"
-    let whereIndex = this.soqlQuery.indexOf("WHERE ");
-    if (
-      whereIndex !== -1 &&
-      whereIndex + "WHERE ".length !== this.soqlQuery.length
-    ) {
-      // If there is something after "WHERE", do nothing
-    } else {
-      // If there is nothing after "WHERE", remove "WHERE" from the string
-      this.soqlQuery = this.soqlQuery.replace(" WHERE", "");
+    } catch (err) {
+      console.error(err);
     }
-
-    this.dispatchEvent(
-      new CustomEvent("fetchresults", {
-        detail: {
-          payload: {
-            soqlQuery: this.soqlQuery,
-          },
-        },
-      })
-    );
   }
 
   formatClause(field, operator, value, type) {
@@ -282,5 +517,13 @@ export default class DynamicFilter extends LightningElement {
       let payload = event.detail.payload;
       this.filters[payload.index].selectedValues = payload.values;
     }
+  }
+
+  handleLogicOptionChange(event) {
+    this.selectedLogicOption = event.detail.value;
+  }
+
+  handleCustomLogicChange(event) {
+    this.customLogic = event.detail.value;
   }
 }
--- force-app/main/default/lwc/queryEditor/queryEditor.js
+++ force-app/main/default/lwc/queryEditor/queryEditor.js
@@ -3,6 +3,7 @@ import getAllObject from "@salesforce/apex/QueryEditorController.getAllObject";
 import getAllfields from "@salesforce/apex/QueryEditorController.getObjectFields";
 import fetchData from "@salesforce/apex/QueryEditorController.fetchData";
 import { ShowToastEvent } from "lightning/platformShowToastEvent";
+import { getObjectInfo } from "lightning/uiObjectInfoApi";
 
 export default class QueryEditor extends LightningElement {
   selectedObject = "";
@@ -13,6 +14,108 @@ export default class QueryEditor extends LightningElement {
   selectedFields = "";
   @track selectedColumnsMap = [];
   @track queryData = [];
+  @track columnsToSearch = [];
+  @track fieldPickerStyle;
+  @track fieldDetails = [];
+  @track objectInfo;
+  orderByField = "";
+  orderByDirection = "";
+  isLoading = false;
+
+  orderByDirections = [
+    { label: "ASC", value: "ASC" },
+    { label: "DESC", value: "DESC" }
+  ];
+
+  limit;
+
+  labels = {
+    chooseFields: "Return which fields:",
+    availableFields: "Add fields",
+    generatedQuery: "Return Records meeting the following conditions:",
+    whereClauses: "Create the where clauses to your query below",
+    orderBy: "Order the number of results by:",
+    incompatibleObject:
+      "The soql string that was passed in was incompatible with the provided object type name",
+    lockObjectButNoSoqlNoObject:
+      "You need to either specify the object type, pass in an existing soql string, or allow the user to choose the object type",
+    buttonRemoveAll: "Remove All"
+  };
+
+  get selectedObjectClass() {
+    return !this.selectedObject
+      ? "slds-col slds-size--9-of-12"
+      : "slds-col slds-size--9-of-12 slds-p-left--small slds-p-top_medium slds-border_top slds-border_right slds-border_bottom slds-border_left";
+  }
+
+  handleFieldSelected(event) {
+    // this.notifyAssignee = event.target.checked === true;
+    console.log("@@ event.detail.value " + event.detail.value);
+    this._selectedFields = this.toggle(
+      this._selectedFields,
+      event.detail.value,
+      true
+    );
+    //this.prepareFieldDescriptors();
+    //this.buildQuery();
+  }
+
+  handleRemoveAll(event) {
+    this._selectedFields = [];
+    this.prepareFieldDescriptors();
+    this._queryString = "";
+    this.buildQuery();
+  }
+
+  handleAddAll(event) {
+    this._selectedFields = this.fieldOptions.map(
+      (curOption) => curOption.value
+    );
+    this.prepareFieldDescriptors();
+    this.buildQuery();
+  }
+
+  handleFieldRemove(event) {
+    this._selectedFields = this.toggle(
+      this._selectedFields,
+      event.detail.value
+    );
+    if (!this._selectedFields || !this._selectedFields.length) {
+      this._queryString = "";
+    }
+    this.prepareFieldDescriptors();
+    this.buildQuery();
+  }
+
+  handleOrderByChanged(event) {
+    this.orderByField = event.detail.value;
+  }
+
+  handleDirectionChanged(event) {
+    this.orderByDirection = event.detail.value;
+  }
+
+  handleLimitChanged(event) {
+    this.limit = event.detail.value;
+  }
+
+  toggle(array, element, skipIfPersists) {
+    if (array && element) {
+      if (array.includes(element)) {
+        if (skipIfPersists) {
+          this.flashSelectedField(element);
+          return array;
+        } else {
+          return array.filter((curElement) => curElement != element);
+        }
+      } else {
+        array.push(element);
+        return array;
+      }
+    } else {
+      return array;
+    }
+  }
 
   @wire(getAllObject)
   objectDetails({ data, error }) {
@@ -25,17 +128,87 @@ export default class QueryEditor extends LightningElement {
     if (error) console.error(error);
   }
 
+  @wire(getObjectInfo, { objectApiName: "$selectedObject" })
+  _getObjectInfo({ error, data }) {
+    if (error) {
+      showToast("Error", getErrorMessage(error), "error");
+    } else if (data) {
+      this.isLoading = true;
+      this.objectInfo = data;
+      const allowedDataTypes = ["String", "Currency"];
+
+      console.log("@@ data " + JSON.stringify(data));
+      this.fieldDetails = Object.values(data.fields).map((field) => ({
+        label: `${field.label}`,
+        value: field.apiName,
+        dataType: field.dataType,
+        selected: false
+      }));
+
+      this.fieldOptions = Object.values(data.fields).map((field) => ({
+        label: `${field.label}`,
+        value: field.apiName
+      }));
+
+      this.isLoading = false;
+      /*this.fieldOptions = Object.values(data.fields)
+        .filter((field) => allowedDataTypes.includes(field.dataType))
+        .map((field) => ({
+          label: `${field.label}`,
+          value: field.apiName
+        }));*/
+    }
+  }
+
+  addAllFields() {
+    this.fieldDetails.forEach((field) => {
+      field.selected = true;
+    });
+  }
+
+  removeAllFields() {
+    this.fieldDetails.forEach((field) => {
+      field.selected = false;
+    });
+  }
+
+  get conditionBuilderStyle() {
+    return !this.selectedObject ? "display: none" : "";
+  }
+
+  get fieldOptionsWithNone() {
+    return [...[{ label: "--NONE--", value: "" }], ...this.fieldOptions];
+  }
+
   getFieldDetails() {
     getAllfields({ sObjectName: this.selectedObject })
-      .then((result) => {
-        this.fieldDetails = result;
+      .then((data) => {
+        this.fieldDetails = data;
         this.fieldOptions = [];
         this.selectedColumnsMap = [];
         this.queryData = [];
         this.selectedFields = "";
-        this.fieldDetails.forEach((field) => {
-          this.fieldOptions.push({ label: field.label, value: field.name });
-        });
+        const allowedDataTypes = ["String", "Currency"];
+
+        console.log("@@ data " + JSON.stringify(data));
+        /*this.fieldDetails = Object.values(data.fields).map((field) => ({
+          label: `${field.label}`,
+          value: field.apiName,
+          dataType: field.dataType,
+          selected: false
+        }));*/
+
+        this.fieldOptions = Object.values(data.fields).map((field) => ({
+          label: `${field.label}`,
+          value: field.name
+        }));
+
+        /*this.fieldOptions = Object.values(data.fields)
+          .filter((field) => allowedDataTypes.includes(field.dataType))
+          .map((field) => ({
+            label: `${field.label}`,
+            value: field.apiName
+          }));*/
       })
       .catch((error) => {
         console.error(error);
@@ -43,83 +216,153 @@ export default class QueryEditor extends LightningElement {
           title: "Error",
           message: "Error While Fetching Fields.",
           variant: "error",
-          mode: "dismissable",
+          mode: "dismissable"
         });
         this.dispatchEvent(evt);
       });
   }
 
   handleObjectChange(event) {
-    if (event.detail) {
+    this.selectedObject = event.detail.value;
+    //this.getFieldDetails();
+    /*if (event.detail) {
       let payload = event.detail.payload;
       const value = payload.value;
       this.selectedObject = value;
 
       this.getFieldDetails();
-    }
+    }*/
   }
 
   handleFieldChange(event) {
-    this.selectedFields = event.detail.value;
+    //this.selectedFields = event.detail.value;
+    const itemName = event.target.label;
+    const isChecked = event.target.checked;
+    let index = event.currentTarget.dataset.index;
+    this.fieldDetails[index].selected = isChecked;
+  }
+
+  handleFetchResults(event) {
+    console.log("@@ in r");
+
+    this.template.querySelector("c-dynamic-filter").generateSOQLQuery();
   }
 
   fetchResults(event) {
-    if (event.detail) {
-      let soqlQuery = event.detail.payload.soqlQuery;
+    console.log("@@ in ev " + JSON.stringify(event.detail));
+    try {
+      if (event.detail) {
+        const selectedFieldsString = this.fieldDetails
+          .filter((item) => item.selected) // Filter the objects where checked is true
+          .map((item) => item.value) // Extract the names from the filtered objects
+          .join(", ");
 
-      fetchData({ soqlQuery: soqlQuery })
-        .then((result) => {
-          //this.queryData = result;
+        const selectedColumns = this.fieldDetails.filter((item) => {
+          return item.selected;
+        });
 
-          if (result.length === 0) {
+        let soql =
+          "SELECT Id, " +
+          selectedFieldsString +
+          " FROM " +
+          this.selectedObject +
+          " ";
+        let whereClaue = event.detail.payload.whereClaue;
+
+        let showWhere = event.detail.payload.showWhere;
+
+        if (showWhere) {
+          soql += "WHERE ";
+          soql += whereClaue;
+        }
+
+        if (this.orderByField) {
+          soql += "ORDER BY " + this.orderByField;
+          soql +=
+            " " + this.orderByDirection !== ""
+              ? ` ${this.orderByDirection}`
+              : "ASC";
+        }
+
+        console.log("@@ li " + this.limit);
+        if (this.limit) {
+          soql += " LIMIT ";
+          soql += this.limit;
+        } else {
+          soql += " LIMIT 50000";
+        }
+
+        //soqlQuery += " LIMIT 50000";
+
+        console.log("@@ soql " + soql);
+
+        fetchData({ soqlQuery: soql })
+          .then((result) => {
+            //this.queryData = result;
+
+            if (result.length === 0) {
+              const evt = new ShowToastEvent({
+                title: "Info",
+                message: "No Records Found.",
+                variant: "info",
+                mode: "dismissable"
+              });
+              this.dispatchEvent(evt);
+              this.queryData = [];
+              return;
+            }
+
+            /*let columnsToDisplay = this.fieldOptions.filter((obj) =>
+              this.selectedFields.includes(obj.value)
+            );
+
+            this.columnsToSearch = columnsToDisplay;
+*/
+            this.selectedColumnsMap = [];
+            selectedColumns.forEach((column) => {
+              this.selectedColumnsMap.push({
+                label: column.label,
+                fieldName: column.value
+              });
+            });
+
+            let allrecords = [];
+            this.queryData = [];
+            for (let i = 0; i < result.length; i++) {
+              let rowData = result[i];
+              let row = rowData;
+              row.Id = rowData.Id;
+              for (let col in this.selectedColumnsMap) {
+                if (col.fieldName != "Id") {
+                  row[col.fieldName] = rowData[col.fieldName];
+                }
+              }
+              allrecords.push(row);
+            }
+
+            this.queryData = allrecords;
+            console.log("@@ query data " + JSON.stringify(this.queryData));
+            console.log(
+              "@@ selectedColumnsMap " + JSON.stringify(this.selectedColumnsMap)
+            );
+          })
+          .catch((error) => {
+            console.error(error);
             const evt = new ShowToastEvent({
-              title: "Info",
-              message: "No Records Found.",
-              variant: "info",
-              mode: "dismissable",
+              title: "Error",
+              message: "Unable To Fetch Data",
+              variant: "error",
+              mode: "dismissable"
             });
             this.dispatchEvent(evt);
-            this.queryData = [];
-            return;
-          }
-
-          let columnsToDisplay = this.fieldOptions.filter((obj) =>
-            this.selectedFields.includes(obj.value)
-          );
-
-          this.selectedColumnsMap = [];
-          columnsToDisplay.forEach((column) => {
-            this.selectedColumnsMap.push({
-              label: column.label,
-              fieldName: column.value,
-            });
           });
-
-          let allrecords = [];
-          for (let i = 0; i < result.length; i++) {
-            let rowData = result[i];
-            let row = rowData;
-            row.Id = rowData.Id;
-            for (let col in this.selectedColumnsMap) {
-              if (col.fieldName != "Id") {
-                row[col.fieldName] = rowData[col.fieldName];
-              }
-            }
-            allrecords.push(row);
-          }
-
-          this.queryData = allrecords;
-        })
-        .catch((error) => {
-          console.error(error);
-          const evt = new ShowToastEvent({
-            title: "Error",
-            message: "Unable To Fetch Data",
-            variant: "error",
-            mode: "dismissable",
-          });
-          this.dispatchEvent(evt);
-        });
+      }
+    } catch (error) {
+      console.error(error);
     }
+  }
+
+  get showDatatable() {
+    return this.queryData.length > 0;
   }
 }
